e9ba21a38da2fbe2803d30d0c45458a03b7fab68
Lemuel Colmenares <leelcoma@gmail.com>
2023-04-20 11:13:51 -0600

    actualizar

---
+++ medianaa.js
@@ -0,0 +1,66 @@
+function calcularMediaAritmetica(lista){
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento; 
+
+        }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+function calcularMediana() {
+    const entradaMediana = document.getElementById("inputMediana");
+    const medianaValue = entradaMediana.value;
+    let arrayMediana = array.from(medianaValue.split(","), Number);
+}
+
+const listaOrdenada = arrayMediana.sort((numMenor, numMayor) => numMenor - numMayor);
+const mitadLista = parseInt(listaOrdenada.length / 2);
+
+function esPar(numerito) {
+    if (numerito % 2 === 0) {
+        return true;
+    }else{
+        return false
+    }
+}
+
+
+
+/* 
+const lista1 = [
+   100,
+   200,
+   500,
+   40000,
+];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+
+function esPar(numerito) {
+   if (numerito % 2 === 0){
+   return true;
+   } else {
+       return false;
+   }
+}
+
+let mediana;
+
+
+if (esPar (lista1.length)){
+   const elemento1 =  lista1[mitadLista1 - 1];
+   const elemento2 = lista1[mitadLista1];   
+   
+   
+   const promedioElemento1y2 = calcularMediaAritmetica([
+       elemento1,
+       elemento2,
+   ]);
+
+   mediana = promedioElemento1y2;
+} else {
+   mediana = lista1[mitadLista1];
+}
+*/
